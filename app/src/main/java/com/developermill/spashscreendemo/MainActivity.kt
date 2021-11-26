15c166efa9b8f840e5e907ab71f6a3610eeb094e
roshan mahajan <roshanmahajan93@gmail.com>
2021-11-27 00:09:53 +0530

    first push

---
+++ app/src/main/java/com/developermill/spashscreendemo/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.developermill.spashscreendemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
